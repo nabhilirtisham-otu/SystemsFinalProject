4b6fc75a31df1f91216956791a3c5b6a42dc52a1
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-24 01:05:08 -0500

    working on taskRunner

--- bin/taskRunner.sh
+++ bin/taskRunner.sh
@@ -1,51 +1,51 @@
-# Executes a task - incl. timeout, retrying, logging functionality
+#Executes a task - incl. timeout, retrying, logging functionality
 
 set -euo pipefail
 
-# Load the configuration file defined in configs/default.conf
+#Load configuration file defined in configs/default.conf
 SELFDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"			# Directory of the current file
 CONFIG="/mnt/c/Users/Nabhi/SystemsFinalProject/configs/default.conf"
 if [ -f "$CONFIG" ]; then						# If the CONFIG file exists, execute it
 	source "$CONFIG"
 fi
 
-# Defines how the file should be used, optionally letting users specify the current run to be a dry run
+#Defines how the file should be used, optionally letting users run a dry run
 defineUsage(){
 	echo "Usage: $0 <task-metadata-file> [--dry-run]"
 	exit 2
 }
 
-# Error handling if an insufficient number of arguments are provided
+#Error handling if insufficient # of arguments provided
 if [ $# -lt 1 ]; then
 	usage
 fi
 
-# Stores the meta file and sets whether the command is a dry run or not
+#Stores meta file, sets if command is dry run or not
 taskMetaFile="$1"
 runDry=false
 if [ "${2:-}" == "--dry-run"]; then
 	runDry=true
 fi
 
-# Error handling if the specified task metadata file isn't found
-# Redirects output to standdard error and exits with code 3 to indicate invalid path
+#Error handling if specified task metadata file isn't found
+#Redirects output to stderr, exits w/ code 3 to indicate invalid path
 if [ ! -f "$taskMetaFile" ]; then
 	echo "Task metadata file not found: $taskMetaFile">&2
 	exit 3
 fi
 
-# Reads a .task metadata file and stores its information in key-value pairs
-declare -A metaInfo							# Declare an indexed associative array	
-while IFS='=' read -r key value; do					# Read lines, elements before '=' go in key, after in value
-	[[ $key =~ ^\s*# ]] continue					# Read lines literally and ignore comments
-	key=$(echo "$key" | tr -d ' \t"')				# Remove spaces, tabs, and quotes
-	value=$(echo "$value" | sed -e 's/^\s*//' -e 's/\s*$//' -e 's/^"//' -e 's/"$//') # Removes leading/trailing spaces and quotes
-	if [ -n "$key" ]; then						# Stores the key-value in the array only if non-empty
+#Reads .task metadata file, stores its info in key-value pairs
+declare -A metaInfo							#Declare indexed associative array	
+while IFS='=' read -r key value; do					#Read lines, elements before '=' go in key, after in value
+	[[ $key =~ ^\s*# ]] continue					#Read lines literally, ignore comments
+	key=$(echo "$key" | tr -d ' \t"')				#Remove spaces, tabs, + quotes
+	value=$(echo "$value" | sed -e 's/^\s*//' -e 's/\s*$//' -e 's/^"//' -e 's/"$//') 	#Removes leading/trailing spaces + quotes
+	if [ -n "$key" ]; then						#Stores key-value in array only if non-empty
 		metaInfo[$key]="$value"
 	fi
-done < "$taskMetaFile"							# Loop reads from the taskMetaFile
+done < "$taskMetaFile"							#Loop reads from taskMetaFile
 
-# Maps array values into local variables
+#Map array values into local vars.
 tID="${metaInfo[tID]:-unnamedTask}"
 tCOMMAND="${metaInfo[tCOMMAND]}"
 tTIMEOUT="${metaInfo[tTIMEOUT]:-${defaultTIMEOUT:-30}}"
@@ -54,13 +54,13 @@ tRETRYDELAY="${metaInfo[tRETRYDELAY]:-${defaultRETRYDELAY:-5}}"
 tNOTIFYSUCCESS="${metaInfo[tNOTIFYSUCCESS]:-${defaultNOTIFYSUCCESS:-false}}"
 tNOTIFYFAILURE="${metaInfo[tNOTIFYFAILURE]:-${defaultNOTIFYFAILURE:-true}}"
 
-# Creates an id for the current run and stores it in the the log file
+#Creates id for current run, stores it in log file
 rID=$(date +%Y%m%d-%H%M%S)
 logDirectory="${LOGS:-/mnt/c/Users/Nabhi/Downloads/SystemsFinalProject}/logs/tasks/${tID}"	# Creates the directory for log files
 mkdir -p "$logDirectory"
 logFile="$logDirectory/${tID}run-${ID}.log"							# Creates the logFile 
 
-# Logs run information in the log file
+#Logs run info in log file
 echo "tID=$tID" > "${logFile}"
 echo "rID=$rID" > "${logFile}"
 echo "tCOMMAND=$tCOMMAND" > "${logFile}"
@@ -69,11 +69,14 @@ echo "tRETRIES=$tRETRIES" > "${logFile}"
 echo "tRETRYDELAY=$tRETRYDELAY" > "${logFile}"
 echo "startTime=$(date --iso-8601=seconds)" >> "${logFile}"
 
-# Prints dry run output
+#Prints dry run output
 if [ "$runDry" == true ]; then
 	echo "A dry run would run: ${tCOMMAND}"
 	echo "Dry run log path: ${logFile}"
 	exit 0
 fi
 
-
+attempt=0
+exitCode=0
+while [ $attempt -le "$tRetries" ]; do				#Checks if the current attempt number is not over the max amount of tries
+	attempt=$((attempt+1))							#Increment attempt counter
