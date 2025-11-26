c49f49c750e1f37d5b3de4f0d68cf8114622ca0d
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-26 07:45:38 -0500

    scheduler.sh

--- bin/scheduler.sh
+++ bin/scheduler.sh
@@ -47,3 +47,30 @@ if [ -f "/mnt/c/Users/Nabhi/Downloads/SystemsFinalProject/tasks/${twID}.task" ];
   CMD="/mnt/c/Users/Nabhi/Downloads/SystemsFinalProject/bin/taskRunner.sh /mnt/c/Users/Nabhi/Downloads/SystemsFinalProject/tasks/${twID}.task"          #Run taskRunner instead
 fi
 
+crENTRY="$crSCHEDULE $crCMD >/dev/null 2>&1"        #Build actual cron line
+
+#Simulate dry run results
+if [ "$dryRun" = true ]; then
+  echo "[DRY RUN] Cron entry: $crENTRY"
+  exit 0
+fi
+
+#Remove mode logic
+if [ "$crREMOVE" = true ]; then
+  crontab -l | grep -v -F "$crCMD" | crontab -          #Take existing user crontab, filter out lines w/ command string, read updated lines from stdin, install as new crontab
+  echo "Cron entry removed for $twID."              #Print confirmation
+  exit 0
+fi
+
+#Install into user/system cron
+if [ "$userCron" = true ]; then         #Check if using user personal crontab
+  (crontab -l 2>/dev/null | grep -v -F "$crCMD" || true; echo "$crENTRY") | crontab -          #List current crontab (errors redirected), remove existing line using same command, appends new cronline, installs updated crontab
+  echo "Installed user cron entry."
+else
+  if [ ! -w /etc/cron.d ]; then         #Check if permission to write to /etc/cron.d given
+    echo "Cannot write to /etc/cron.d (need root privileges)." >&2          #Error message if not
+    exit 1
+  fi
+  echo "$crENTRY" > "/etc/cron.d/task_scheduler_${twID}"            #Write cron file in /etc/cron.d w/ name based on twID, define scheduled job for cron to do
+  echo "Installed system cron entry /etc/cron.d/task_scheduler_${twID}"
+fi
