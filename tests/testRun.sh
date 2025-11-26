2332f14024f96d3272c9eeb3b03628f67cdfc4f5
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-26 08:28:09 -0500

    testRun.sh

---
+++ tests/testRun.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail           #Error-handling options (strict)
+
+baseDir="/mnt/c/Users/Nabhi/Downloads/SystemsFinalProject"              #Points to root dir
+binDir="$baseDir/bin"                   #Point to bin folder in project
+
+#Runs successful task
+echo "Running sampleGood.task (succeeds)"
+$binDir/taskRunner.sh "$baseDir/tasks/sampleGood.task"
+
+#Runs failing task
+echo "Running sampleBad.task (fails once, then succeeds w/ retries)"
+$binDir/taskRunner.sh "$baseDir/tasks/sampleBad.task"
+
+#Runs sequential workflow
+echo "Running sampleFlow.workflow"
+$binDir/workflowRunner.sh "$baseDir/workflows/sampleFlow.workflow"
